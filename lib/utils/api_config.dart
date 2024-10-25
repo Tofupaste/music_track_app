12198f88aa8cff1d3f96cfca1c3c06608ab16880
Tofupaste <abiputra3566@gmail.com>
2024-10-25 21:45:10 +0700

    youtube_api

--- lib/utils/api_config.dart
+++ lib/utils/api_config.dart
@@ -1,13 +1,12 @@
 class ApiConfig {
   // Spotify API configurations
   static const String spotifyBaseUrl = 'https://api.spotify.com/v1';
-  static const String spotifyAccessToken = 'YOUR_SPOTIFY_ACCESS_TOKEN'; // Masukkan token akses Spotify di sini
   
   // YouTube API configurations
   static const String youtubeBaseUrl = 'https://www.googleapis.com/youtube/v3';
-  static const String youtubeApiKey = 'YOUR_YOUTUBE_API_KEY'; // Masukkan API key YouTube di sini
+  static const String youtubeApiKey = 'Enter ur youtube API here'; // Masukkan API key YouTube di sini
 
-  // Headers for Spotify requests
+  // Headers for Spotify requests (token didapatkan secara dinamis)
   static Map<String, String> getSpotifyHeaders(String token) {
     return {
       'Authorization': 'Bearer $token',
