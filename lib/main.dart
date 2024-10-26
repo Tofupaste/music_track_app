87fea454c2ecb4a509f8aeebf2723f9026ac3eaf
Tofupaste <abiputra3566@gmail.com>
2024-10-26 23:02:06 +0700

    main_up_3

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:my_music_app/services/spotify_auth_services.dart'; // Pastikan path ini benar
 import 'providers/artist_provider.dart'; 
 import 'providers/song_provider.dart';   
 import 'screen/search_screen.dart';
 
+void fetchSpotifyToken() async {
+  try {
+    String accessToken = await SpotifyAuthService.getAccessToken();
+    print('Access Token: $accessToken');
+  } catch (e) {
+    print('Error: $e');
+  }
+}
+
 void main() {
+  // Panggil fetchSpotifyToken() jika ingin melihat token
+  fetchSpotifyToken();
+
   runApp(const MyApp());
 }
 
