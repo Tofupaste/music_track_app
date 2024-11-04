6d85a3c5b63da088cffe1018f781acf3c047ee24
Tofupaste <abiputra3566@gmail.com>
2024-11-05 00:14:45 +0700

    services_up_4

--- lib/services/youtube_api_service.dart
+++ lib/services/youtube_api_service.dart
@@ -1,27 +1,45 @@
-import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import '../utils/api_config.dart';
+import 'dart:convert';
+import '../utils/formatter.dart'; 
+import '../utils/api_config.dart'; 
 
 class YouTubeApiService {
-  static Future<List<Map<String, dynamic>>> searchVideos(String query) async {
-    final url = '${ApiConfig.youtubeBaseUrl}/search?part=snippet&q=$query&type=video&key=${ApiConfig.youtubeApiKey}';
-    try {
-      final response = await http.get(Uri.parse(url));
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
-        final List<dynamic> videosJson = data['items'];
-        return videosJson.map((video) {
-          return {
-            'videoId': video['id']['videoId'],
-            'title': video['snippet']['title'],
-            'thumbnailUrl': video['snippet']['thumbnails']['high']['url'],
-          };
-        }).toList();
-      } else {
-        throw Exception('Failed to search videos: ${response.statusCode}');
+  static Future<List<Map<String, dynamic>>> searchVideos(String artistName) async {
+    final url = Uri.parse(ApiConfig.getYouTubeSearchUrl(artistName));
+    final response = await http.get(url);
+    
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      List<Map<String, dynamic>> videos = [];
+
+      for (var item in data['items'] ?? []) {
+        final videoId = item['id']['videoId'];
+        
+        // Fetch view count for each video
+        final videoDetailsUrl = Uri.parse(ApiConfig.getYouTubeVideoDetailsUrl(videoId));
+        final videoResponse = await http.get(videoDetailsUrl);
+        
+        if (videoResponse.statusCode == 200) {
+          final videoData = json.decode(videoResponse.body);
+          final viewCount = int.tryParse(videoData['items']?[0]['statistics']?['viewCount'] ?? '0') ?? 0;
+          
+          videos.add({
+            'videoId': videoId,
+            'title': item['snippet']['title'],
+            'thumbnailUrl': item['snippet']['thumbnails']['default']['url'],
+            'viewCount': viewCount,
+          });
+          
+          debugPrint("Video: ${item['snippet']['title']} - ViewCount: $viewCount");
+        } else {
+          debugPrint("Failed to fetch video details for videoId: $videoId");
+        }
       }
-    } catch (error) {
-      throw Exception('An error occurred: $error');
+      
+      return videos;
+    } else {
+      throw Exception('Failed to load videos');
     }
   }
 }
