ba802095d6838c71f751d0fa7cd20db29126c106
Tofupaste <abiputra3566@gmail.com>
2024-11-05 00:10:52 +0700

    utils_up_1

--- lib/utils/api_config.dart
+++ lib/utils/api_config.dart
@@ -1,11 +1,11 @@
 class ApiConfig {
   // Spotify API configurations
   static const String spotifyBaseUrl = 'https://api.spotify.com/v1';
-  
+
   // YouTube API configurations
   static const String youtubeBaseUrl = 'https://www.googleapis.com/youtube/v3';
-  static const String youtubeApiKey = 'Enter ur youtube API here'; // Masukkan API key YouTube di sini
-
+  static const String youtubeApiKey = 'AIzaSyAx7wbaoaqzPdJeeOzV2fge5jiY3rgWulU'; // Ganti dengan API key YouTube Anda
+  
   // Headers for Spotify requests (token didapatkan secara dinamis)
   static Map<String, String> getSpotifyHeaders(String token) {
     return {
@@ -14,12 +14,21 @@ class ApiConfig {
     };
   }
 
-  // URLs
+  // Spotify API URLs
   static String getSearchArtistUrl(String query) {
     return '$spotifyBaseUrl/search?q=$query&type=artist';
   }
 
   static String getTopTracksUrl(String artistId) {
     return '$spotifyBaseUrl/artists/$artistId/top-tracks';
+  }
+
+  // YouTube API URLs
+  static String getYouTubeSearchUrl(String query) {
+    return '$youtubeBaseUrl/search?part=snippet&q=$query&type=video&key=$youtubeApiKey';
+  }
+
+  static String getYouTubeVideoDetailsUrl(String videoId) {
+    return '$youtubeBaseUrl/videos?part=statistics&id=$videoId&key=$youtubeApiKey';
   }
 }
