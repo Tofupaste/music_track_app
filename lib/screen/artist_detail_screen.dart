581408df29633bec496abb4ed34eff8a1820384c
Tofupaste <abiputra3566@gmail.com>
2024-11-06 22:10:24 +0700

    screen_up_4

--- lib/screen/artist_detail_screen.dart
+++ lib/screen/artist_detail_screen.dart
@@ -3,41 +3,59 @@ import 'package:provider/provider.dart';
 import '../widgets/song_card.dart';
 import '../providers/song_provider.dart';
 
-class ArtistDetailScreen extends StatelessWidget {
+class ArtistDetailScreen extends StatefulWidget {
   final String artistId;
-  final String artistName; // Menambahkan parameter untuk nama artis
+  final String artistName;
 
-  const ArtistDetailScreen({Key? key, required this.artistId, required this.artistName}) : super(key: key);
+  const ArtistDetailScreen({
+    Key? key,
+    required this.artistId,
+    required this.artistName,
+  }) : super(key: key);
+
+  @override
+  _ArtistDetailScreenState createState() => _ArtistDetailScreenState();
+}
+
+class _ArtistDetailScreenState extends State<ArtistDetailScreen> {
+  bool _isInitialized = false; // Tambahan flag untuk memastikan fetchSongs hanya sekali
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (!_isInitialized) {
+      Provider.of<SongProvider>(context, listen: false)
+          .fetchSongs(widget.artistId, widget.artistName);
+      _isInitialized = true; // Set flag agar fetchSongs tidak dipanggil lagi
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    final songProvider = Provider.of<SongProvider>(context, listen: false);
-    
-    // Fetch songs ketika screen dibuka
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      songProvider.fetchSongs(artistId, artistName);
-    });
-
     return Scaffold(
       appBar: AppBar(
-        title: Text('$artistName - Top Tracks'), // Tampilkan nama artis di judul
+        title: Text('${widget.artistName} - Top Tracks'),
       ),
       body: Consumer<SongProvider>(
         builder: (context, songProvider, child) {
-          return songProvider.isLoading
-              ? const Center(child: CircularProgressIndicator())
-              : ListView.builder(
-                  itemCount: songProvider.songs.length,
-                  itemBuilder: (context, index) {
-                    final song = songProvider.songs[index];
-                    return SongCard(
-                      song: song,
-                      onTap: () {
-                        // Logic ketika song card ditekan (misalnya, putar lagu atau tampilkan detail lagu)
-                      },
-                    );
+          if (songProvider.isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (songProvider.errorMessage.isNotEmpty) {
+            return Center(child: Text(songProvider.errorMessage)); // Menampilkan error message jika ada
+          } else {
+            return ListView.builder(
+              itemCount: songProvider.songs.length,
+              itemBuilder: (context, index) {
+                final song = songProvider.songs[index];
+                return SongCard(
+                  song: song,
+                  onTap: () {
+                    // Logic ketika song card ditekan
                   },
                 );
+              },
+            );
+          }
         },
       ),
     );
