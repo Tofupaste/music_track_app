3593b74e158c95ee665f987fa9ed0a52051547af
Tofupaste <abiputra3566@gmail.com>
2024-11-10 15:51:32 +0700

    widget_up1_2

--- lib/widgets/song_card.dart
+++ lib/widgets/song_card.dart
@@ -4,41 +4,27 @@ import '../models/song.dart';
 class SongCard extends StatelessWidget {
   final Song song;
   final VoidCallback onTap;
-  final String selectedPlatform; // Tambahkan parameter platform
+  final String selectedPlatform;
 
   const SongCard({
     Key? key,
     required this.song,
     required this.onTap,
-    required this.selectedPlatform, // Tambahkan parameter platform
+    required this.selectedPlatform,
   }) : super(key: key);
-
-  String formatListenerCount(int count) {
-    if (count >= 1000000) {
-      return '${(count / 1000000).toStringAsFixed(1)}M'; // Format jutaan dengan 1 desimal
-    } else if (count >= 1000) {
-      return '${(count / 1000).toStringAsFixed(1)}K'; // Format ribuan dengan 1 desimal
-    } else {
-      return count.toString(); // Tampilkan angka secara langsung jika di bawah 1000
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      onTap: onTap,
-      leading: song.albumImageUrl != null
-          ? Image.network(song.albumImageUrl!)
-          : const Icon(Icons.music_note),
-      title: Text(song.title),
-      subtitle: Text(song.albumName),
-      trailing: Text(
-        selectedPlatform == 'Spotify'
-            ? 'Spotify: ${formatListenerCount(song.spotifyListening)}'
-            : 'YouTube: ${formatListenerCount(song.youtubeListening)}',
-        style: TextStyle(
-          color: selectedPlatform == 'Spotify' ? Colors.green : Colors.red,
-        ),
+    // Menampilkan jumlah pendengar yang sesuai berdasarkan platform
+    final listenerCount = selectedPlatform == 'Spotify'
+        ? song.spotifyListening
+        : song.youtubeListening;
+
+    return Card(
+      child: ListTile(
+        title: Text(song.title),
+        subtitle: Text('$selectedPlatform - $listenerCount views'), // Menampilkan view count yang sesuai
+        onTap: onTap,
       ),
     );
   }
