25dba22aaf3d3a04e9e27ea0fe289952369cc51c
Tofupaste <abiputra3566@gmail.com>
2024-11-11 20:33:25 +0700

    widgets_up1_3

--- lib/widgets/song_card.dart
+++ lib/widgets/song_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/song.dart';
+import '../utils/formatter.dart';
 
 class SongCard extends StatelessWidget {
   final Song song;
@@ -15,15 +16,26 @@ class SongCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Menampilkan jumlah pendengar yang sesuai berdasarkan platform
     final listenerCount = selectedPlatform == 'Spotify'
         ? song.spotifyListening
         : song.youtubeListening;
 
     return Card(
       child: ListTile(
+        leading: song.albumImageUrl != null
+            ? FadeInImage.assetNetwork(
+                placeholder: 'assets/images/loading.png',
+                image: song.albumImageUrl!,
+                fit: BoxFit.cover,
+                width: 100,
+                height: 100,
+                imageErrorBuilder: (context, error, stackTrace) {
+                  return Image.asset('assets/images/music_cross.png', fit: BoxFit.cover);
+                },
+              )
+            : Image.asset('assets/images/music_cross.png', fit: BoxFit.cover),
         title: Text(song.title),
-        subtitle: Text('$selectedPlatform - $listenerCount views'), // Menampilkan view count yang sesuai
+        subtitle: Text('$selectedPlatform - ${formatNumber(listenerCount)} views'),
         onTap: onTap,
       ),
     );
