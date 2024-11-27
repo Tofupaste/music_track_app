e4ea3850f2299a96cec5f8b556a16f7ca3c61a10
Tofupaste <abiputra3566@gmail.com>
2024-11-27 22:51:44 +0700

    screen_up2_1

--- lib/screen/artist_detail_screen.dart
+++ lib/screen/artist_detail_screen.dart
@@ -15,6 +15,7 @@ class ArtistDetailScreen extends StatefulWidget {
 
 class _ArtistDetailScreenState extends State<ArtistDetailScreen> {
   String? _selectedPlatform;
+  String _displayMode = 'Popular'; // Default mode is "Popular"
 
   @override
   void initState() {
@@ -61,11 +62,38 @@ class _ArtistDetailScreenState extends State<ArtistDetailScreen> {
     );
   }
 
+  void _toggleDisplayMode() {
+    // Toggle antara Popular dan Most
+    setState(() {
+      _displayMode = _displayMode == 'Popular' ? 'Most' : 'Popular';
+      final songProvider = Provider.of<SongProvider>(context, listen: false);
+      songProvider.clearSongs();
+
+      // Fetch data berdasarkan mode dan platform
+      if (_displayMode == 'Popular') {
+        songProvider.fetchSongs(widget.artistId, widget.artistName, _selectedPlatform!);
+      } else {
+        songProvider.setDummyMostSongs(); // Placeholder untuk Most
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('${widget.artistName} - Top Tracks (${_selectedPlatform ?? ""})'),
+        actions: [
+          // Tombol toggle akan terlihat untuk semua platform
+          if (_selectedPlatform != null)
+            TextButton(
+              onPressed: _toggleDisplayMode,
+              child: Text(
+                _displayMode,
+                style: const TextStyle(color: Color.fromARGB(255, 5, 230, 247)),
+              ),
+            ),
+        ],
       ),
       body: Consumer<SongProvider>(
         builder: (context, songProvider, child) {
@@ -83,7 +111,7 @@ class _ArtistDetailScreenState extends State<ArtistDetailScreen> {
                         return SongCard(
                           song: song,
                           onTap: () {
-                            // Logic ketika song card ditekan (misalnya, putar lagu atau tampilkan detail lagu)
+                            // Logic untuk ketika song card ditekan
                           },
                           selectedPlatform: _selectedPlatform!,
                         );
